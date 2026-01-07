066876cd63435e809bc084fbe94c30f28a3fe4fd
hahahaha88888 <foojeremy4@gmail.com>
2026-01-07 21:39:15 +0800

    Update run.sh

--- TiDB/scripts/run.sh
+++ TiDB/scripts/run.sh
@@ -1,6 +1,5 @@
 ./bin/ycsb.sh run jdbc \
   -P workloads/workloada \
-  -p db.url=jdbc:mysql://127.0.0.1:4000/ycsb \
-  -p db.user=root \
-  -p db.passwd= \
-  -p db.driver=com.mysql.cj.jdbc.Driver
+  -P config/tidb.properties \
+  -p recordcount=10000 \
+  -p operationcount=10000
